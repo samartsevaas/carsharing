90300e61d6063c56741049d2d6481b12c81c139c
samartsevaas <55296490+samartsevaas@users.noreply.github.com>
2021-09-27 15:45:03 +0300

    Revert "V-01 final"

--- src/main.js
+++ src/main.js
@@ -2,35 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./styles/main.scss";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faUserSecret,
-  faMapMarkerAlt,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faInstagram,
-  faTelegram,
-  faFacebook,
-} from "@fortawesome/free-brands-svg-icons";
-
-
-library.add(faUserSecret);
-library.add(faTelegram);
-library.add(faFacebook);
-library.add(faInstagram);
-library.add(faMapMarkerAlt);
-import EasySlider from 'vue-easy-slider'
- 
-Vue.use(EasySlider)
-
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,5 @@
-const path = require("path");
 module.exports = {
   publicPath: "/carsharing/",
-  css: {
-    loaderOptions: {
-      scss: {
-        additionalData: `@import "~@/styles/abstracts/index.scss";`,
-      },
-    },
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-        "@shared": path.resolve(__dirname, "src/components/shared"),
-        "@pages": path.resolve(__dirname, "src/components/pages"),
-        "@modules": path.resolve(__dirname, "src/components/modules"),
-      },
-    },
-  },
+
+  transpileDependencies: ["vuetify"],
 };
